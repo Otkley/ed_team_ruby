1ee955c0d9f8a83e3e0198be8fa345449e337a8b
Sergio Montiel <sergiomontielcastello@gmail.com>
2023-11-21 20:02:32 -0600

    Un intento de seguir el proyecto del curso hecho en rails 6 pero con rails 7, se lograron algunas cosas pero otras no y se dejaron para continuar con otro curso mas actualizado

---
+++ friends-app/db/seeds.rb
@@ -0,0 +1,12 @@
+require 'faker'
+
+10.times do
+
+  Friend.create!(
+    first_name: Faker::Name.name,
+    last_name: Faker::Name.last_name,
+    phone_number: Faker::PhoneNumber.phone_number,
+    address: Faker::Address.full_address
+  )
+
+end
